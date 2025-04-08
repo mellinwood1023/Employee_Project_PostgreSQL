e715890dc95397d2381869686dca423c39a23f86
Maggie Ellinwood <mellinwood1023@gmail.com>
2025-04-08 11:20:03 -0400

    updating server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-import { QueryResult } from 'pg';
 import { pool, connectToDb } from './connection.js';
-
 
 await connectToDb();
 
@@ -16,21 +14,34 @@ app.get('/', (req, res) => {
 });
 
 app.get('/employees', async (req, res) => {
-    const pool = new Pool();
+try {
     const result = await pool.query('SELECT * FROM employees');
     res.send(result.rows);
+} catch (error) {
+    console.error('Error fetching employees:', error);
+    res.status(500).send('Internal Server Error');
+}
 });
 
 app.get('/employees/:id', async (req, res) => {
-    const pool = new Pool();
+   try {
     const result = await pool.query('SELECT * FROM employees WHERE id = $1', [req.params.id]);
     res.send(result.rows);
+   } catch (error) {
+    console.error('Error fetching employee:', error);
+    res.status(500).send('Internal Server Error');
+   }
 });
 
 app.post('/employees', async (req, res) => {
-    const pool = new Pool();
-    const result = await pool.query('INSERT INTO employees (name, age, salary) VALUES ($1, $2, $3)', [req.body.name, req.body.age, req.body.salary]);
-    res.send(result.rows);
+try {
+    const result = await pool.query('INSERT INTO employees (name, age, salary) VALUES ($1, $2, $3)', 
+        [req.body.name, req.body.age, req.body.salary]);
+    res.send(result.rows[0]);
+} catch (error) {
+    console.error('Error adding employee:', error);
+    res.status(500).send('Internal Server Error');
+}
 });
 
 app.use((req, res) => {
